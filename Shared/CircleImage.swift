8b00c956076066581ff2607195847bb909be657b
Praveen Rao <prao76574@gmail.com>
2021-09-04 01:05:59 -0500

    added image view

---
+++ Shared/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  AttendEase
+//
+//  Created by Praveen Rao on 9/4/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}

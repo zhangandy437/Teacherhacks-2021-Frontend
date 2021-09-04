3bc5927c8b74d6fa933a28efa31ded5fea9987e3
Andy Zhang <zhangandy0001@gmail.com>
2021-09-03 22:35:09 -0500

    Initial Commit

---
+++ Shared/AttendEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendEaseApp.swift
+//  Shared
+//
+//  Created by Andy Zhang on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AttendEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
